a781968f3e2562cdec3c60dd7dbdf2be734a49ea
MayuraNaito <mayura19941122@gmail.com>
2023-04-15 16:11:13 +0900

    課題1.22 パッケージをコンパイル

---
+++ Theme2/src/process/Info.java
@@ -0,0 +1,45 @@
+/*
+ 
+	下記がコンソールに出力されるように作成してください
+	
+		こんにちは！ここは日本です！
+		この寿司はうまい
+		寿司は和食です
+		今の現在日時は2023/03/09 10:23:39です
+
+	【条件】
+		・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+		・適切なファイルにフィールドで変数を必要な数作ってください。
+		・thisを使って作成してください。
+		・日時は今日の日付を取得してください。
+		
+  */
+
+package process;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Info {
+	//カレンダークラスを取得しシステム日時を取得。
+	Calendar cal = Calendar.getInstance();
+	//フォーマットクラス
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+	//出力用のテキストを格納。
+	String[] text = {
+			"こんにちは！ここは日本です！", 
+			"この寿司はうまい", 
+			"寿司は和食です"
+			};
+	
+	//テキストを表示するメソッド。
+	public void text() {
+		for(int i = 0; i < text.length; i++) {
+			System.out.println(text[i]);
+		}
+	}
+	
+	//日時を表示するメソッド。
+	public void date() {
+		System.out.println(sdf.format(cal.getTime()));
+	}
+}
